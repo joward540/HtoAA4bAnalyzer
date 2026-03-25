13a749441c24bc7b9c59c73f8bee9e417ed85c9e
Juston Ward <joward540@gmail.com>
2026-03-25 11:28:11 -0500

    Changed output filename for RECO plots

--- examples/HtoaaAna.cxx
+++ examples/HtoaaAna.cxx
@@ -57,7 +57,8 @@ void HtoaaAna() {
   }
   
   // MuMonitor validation example with Muon 2010 dataset
-  string outfile = "HtoaaAna_AK8_Histos.root";              // version NanoAODRun1_v1
+  //string outfile = "HtoaaAna_AK8_Histos.root";              // version NanoAODRun1_v1
+  string outfile = "HtoaaAna_RECO_Histos.root";          
 
   TFile fout((outfile).c_str(),"RECREATE");
 
